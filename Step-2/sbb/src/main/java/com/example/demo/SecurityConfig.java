174ac95555e4207b8d0bfe5d497c5b978eff4401
rnjstngks <rnjstngks267@naver.com>
2025-02-04 10:57:10 +0900

    Update SecurityConfig.java

--- Step-2/sbb/src/main/java/com/example/demo/SecurityConfig.java
+++ Step-2/sbb/src/main/java/com/example/demo/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration //이 파일이 스프링의 환결 설정 파일임을 의미하는 애너테이션 
 @EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션
 // @EnableWebSecurity 애너테이션을 사용하면 스프링 시큐리티를 활성화하는 역할을 합니다.
-@EnableMethodSecurity(prePostEnabled = true) // @PreAuthorize 애너테이션을 사용하기 위한 애너테이션이다.
+//@EnableMethodSecurity(prePostEnabled = true) // @PreAuthorize 애너테이션을 사용하기 위한 애너테이션이다.
 public class SecurityConfig {
 	// Bean은 스프링에 의해 생성 또는 관리되는 객체를 의미
 	@Bean
@@ -27,9 +27,8 @@ public class SecurityConfig {
 		// H2 콘솔은 스프링 시큐리티 CSRF 처리를 예외로 함
 		http
 			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-					.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-			.csrf((csrf) -> csrf
-					.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+			.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+			.csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
 //					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // ✅ CSRF 토큰을 쿠키로 제공
 					)
 			.headers((headers) -> headers
